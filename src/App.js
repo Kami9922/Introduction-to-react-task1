2debcbae125da4eef3269af0e7317e05059e6121
Vitalya <vitalikt_22@mail.ru>
2024-07-27 20:52:00 +0400

    base configuration, feature of current year and separate code styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//Императивный
+const getDateFormat = () => {
+	let date = new Date();
+	let year = date.getFullYear();
+	return year;
+};
+const resultYear = getDateFormat();
 
-export default App;
+//Декларативный
+export const App = () => {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+				{resultYear}
+			</header>
+		</div>
+	);
+};
